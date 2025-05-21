1ecf561b28f60f33d95fdf4052a938a8eec97f5c
Daniel Gruno <humbedooh@apache.org>
2025-05-21 12:19:52 -0500

    switch run user to the app itself
    the app should run as 'webapp' for permission reasons

--- .platform/hooks/postdeploy/pipservice.sh
+++ .platform/hooks/postdeploy/pipservice.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 # This is a Beanstalk pipservice deployment script.
+RUN_AS='webapp'
 # Beanstalk Python apps are deployed to /var/app/current
 cd /var/app/current/
 # Install poetry
 python3.13 -m pip install --upgrade poetry
 # Install dependencies for the service if we find a pyproject.toml file
 if test -f pyproject.toml; then
-    poetry install --no-interaction --no-root
+    runuser ${RUN_AS} -c 'poetry install --no-interaction --no-root'
 fi
 # For each .service file we find here, install it and (re)start the service
 for file in *.service ; do
